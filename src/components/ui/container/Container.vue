23db7dd990444fe7977fb0737d094607d2e6bcce
jethrocadang <116892945+jethrocadang@users.noreply.github.com>
2024-06-07 23:24:12 +0800

    auth:[added login component]

---
+++ src/components/ui/container/Container.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/lib/utils'
+
+const props = defineProps<{
+    class?: HTMLAttributes['class']
+}>()
+</script>
+
+<template>
+    <div :class="cn( 
+        'h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20 bg-green-200' ,props.class
+        )">
+        <slot />
+
+    </div>
+</template>
